2227330e10febb39bbc7f4f297c7300e7a1d6a53
cafetomo <92923510+cafetomo@users.noreply.github.com>
2021-11-17 21:11:25 +0900

    Update InMemoryLandMarkRepository.cs

--- src/applications/movgame.Repository/InMemory/InMemoryLandMarkRepository.cs
+++ src/applications/movgame.Repository/InMemory/InMemoryLandMarkRepository.cs
@@ -12,6 +12,9 @@ namespace movgame.Repository.InMemory
             var result = new Dictionary<int, LandMark>();
             result.Add(0, new LandMark
             {
+                // データが固定になってしまうので本来は使用しない
+                // 以下はJavaScriptの見本だが考え方は一緒
+                // https://lets-csharp.com/pacman-js-complete/
                 MarkRows = new string[] {
                     "＃＃＃＃＃＃＃＃＃＃＃＃",
                     "＃＠＿＿＿＿＃＃＿＿＠＃",
